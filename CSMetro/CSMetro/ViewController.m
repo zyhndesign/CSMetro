2ba8ec7a5d9a99944db67a293bb63b9b289a17fc
liling <maximuslee@126.com>
2014-03-25 11:02:44 +0800

    init project

---
+++ CSMetro/CSMetro/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  CSMetro
+//
+//  Created by 工业设计中意（湖南） on 14-3-25.
+//  Copyright (c) 2014年 潇湘晨报新媒体. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
